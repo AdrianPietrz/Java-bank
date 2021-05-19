464e269b58d1055cd6bd3bbf2dce861bf5a1270c
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-05-19 22:44:31 +0200

    created user structure

---
+++ user.java
@@ -0,0 +1,15 @@
+public class user {
+
+    int user_ID;
+    String name;
+    String surname;
+    double money;
+
+    public user(String name, String surname, double money)
+    {
+        this.name=name;
+        this.surname=surname;
+        this.money=money;
+    }
+
+}

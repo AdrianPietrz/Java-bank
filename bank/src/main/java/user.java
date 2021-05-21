e234552323a44e1e5eca27e7ce9f19973c672f2c
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-05-21 16:26:29 +0200

    need to add pesel field

--- bank/src/main/java/user.java
+++ bank/src/main/java/user.java
@@ -3,49 +3,73 @@ import java.util.Objects;
 import org.bson.types.ObjectId;
 
 
-public final class user {
+public final class User {
 
     private ObjectId user_ID;
     private String name;
     private String surname;
+    private String account_number;
+    private String pin;
     private double money;
 
-    public user(final String name,final String surname,final double money)
+    public User(final String name, final String surname, String account_number, String pin,final double money)
     {
         this.name=name;
         this.surname=surname;
+        this.account_number=account_number;
+        this.pin=pin;
         this.money=money;
     }
-    public user() {}
+    public User() {}
 
     public ObjectId getId() {
         return user_ID;
     }
 
-    public void setId(final ObjectId id) {
-        this.user_ID = id;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
     }
-    public void setSurname(final String surname) {
-        this.surname = surname;
+    public String getPin() {
+        return pin;
     }
-    public double getMoney() {
+    public String getAccount_number() {
+        return account_number;
+    }
+    public Double getMoney() {
         return money;
     }
 
-    public void setMoney(final int money) {
+
+    public User setId(ObjectId id) {
+        this.user_ID = id;
+        return this;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+    public User setPin(String pin) {
+        this.pin = pin;
+        return this;
+    }
+
+    public User setAccount_number(String account_number) {
+        this.account_number = account_number;
+        return this;
+    }
+
+    public User setMoney(Double money) {
         this.money = money;
+        return this;
     }
 
 
@@ -56,11 +80,12 @@ public final class user {
                 + "id='" + user_ID + "'"
                 + ", name='" + name + "'"
                 + ", surname='" + surname + "'"
+                + ", PESEL='" + account_number + "'"
                 + ", money='" + money + "'}";
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(user_ID, name, surname);
+        return Objects.hash(user_ID, name, surname,account_number);
     }
 }

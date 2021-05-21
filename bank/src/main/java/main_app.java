4867d150ecb15e6728615f4622c939909345d807
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-05-21 16:26:08 +0200

    Update main_app.java

--- bank/src/main/java/main_app.java
+++ bank/src/main/java/main_app.java
@@ -1,8 +1,26 @@
+import javax.swing.*;
+
 public class main_app {
 
     public static void main(String args[]) {
         CreateAccount createAccount=new CreateAccount();
-        createAccount.CreateAccount();
+        LandingScreen landingScreen=new LandingScreen();
+        User user=null;
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setVisible(true);
+
+        if(landingScreen.LandingScreen(frame)){
+
+        }
+        else{
+            user=createAccount.Create(frame);
+        }
+
+
+
+
+
     }
 
 

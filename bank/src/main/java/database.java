1ca09c3dac86e3989bea0fc675103957a8b0c7d7
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-05-21 15:17:00 +0200

    connection and adding works

--- bank/src/main/java/database.java
+++ bank/src/main/java/database.java
@@ -1,6 +1,4 @@
-import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -8,18 +6,15 @@ import com.mongodb.client.MongoDatabase;
 
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
-import org.bson.types.ObjectId;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-
 import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Updates.*;
 
 public class database {
 
 
-    MongoCollection<user> userList = null;
+    MongoCollection<User> userList = null;
     MongoDatabase database = null;
     MongoClient Client = null;
 
@@ -32,10 +27,17 @@ public class database {
     }
 
 
-    public void addUser(user a){
-        userList = database.getCollection("User", user.class);
+    public void addUser(User a){
+        userList = database.getCollection("User", User.class);
         userList.insertOne(a);
+        User temp=userList.find(eq("account_number",a.getAccount_number())).first();
+        a.setId(temp.getId());
+    }
 
+    public boolean checkIfUserExist(String account_number){
+        User temp=userList.find(eq("account_number",account_number)).first();
+        if(temp==null) return false;
+        else return true;
     }
 
 }
